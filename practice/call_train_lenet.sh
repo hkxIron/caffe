044615e5ac345b0b4a89be6fda52f5e97f04d407
kexin.hkx <kexin.hkx@test.com>
2017-08-25 20:58:16 +0800

    update

--- practice/call_train_lenet.sh
+++ practice/call_train_lenet.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd $CAFFE_HOME
-sh examples/mnist/train_lenet.sh >&1|tee practice/logs/log_train_lenet
+#sh examples/mnist/train_lenet.sh >&1|tee practice/logs/log_train_lenet
+nohup sh examples/mnist/train_lenet.sh >&1|tee practice/logs/log_train_lenet
